25c463f7210d8e8b72da8641b0967e3388ccaee8
unknown <morpharrant@gmail.com>
2025-11-15 23:00:14 +0300

    fix

--- 2025.11.15-Homework-6/311/311.cpp
+++ 2025.11.15-Homework-6/311/311.cpp
@@ -1,8 +1,25 @@
 #include <cstdio>
 
+double power(double a, int n)
+{
+    if (n == 0){
+        return 1;
+    }
+    double t = power(a, n / 2);
+    if (n % 2 == 0){
+        return t * t;
+    }
+    else {
+        return (t * t * a);
+    }
+}
+
 int main(int argc, char** argv)
 {
-    
-    
+    double a = 0;
+    int n = 0;
+    scanf("%lf %d", &a, &n);
+    printf("%lf", power(a, n));
+
     return 0;
 }
